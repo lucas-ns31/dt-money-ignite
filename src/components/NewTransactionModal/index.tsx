aae2270a3f1084bf184aa771e180539458ab08d7
Lucas Nunes <ns_lucas@hotmail.com>
2023-05-03 21:19:46 -0300

    Projeto de estudo atualizado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,13 @@
+import { FormEvent, useState } from 'react';
+
 import Modal from 'react-modal';
+
+import incomeImg from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
 	isOpen: boolean;
@@ -11,6 +18,26 @@ export function NewTransactionModal({
 	isOpen,
 	onRequestClose,
 }: NewTransactionModalProps) {
+	//estado dos inputs do form
+	const [title, setTitle] = useState('');
+	const [value, setValue] = useState(0);
+	const [category, setCategory] = useState('');
+
+	const [type, setType] = useState('deposit');
+
+	function handleCreateNewTransaction(event: FormEvent) {
+		event.preventDefault();
+
+		const data = {
+			title,
+			value,
+			type,
+			category,
+		};
+
+		api.post('/transactions', data);
+	}
+
 	return (
 		<Modal
 			isOpen={isOpen}
@@ -26,14 +53,49 @@ export function NewTransactionModal({
 				<img src={closeImg} alt="Fechar modal" />
 			</button>
 
-			<Container>
+			<Container onSubmit={handleCreateNewTransaction}>
 				<h2>Cadastrar transação</h2>
 
-				<input placeholder="Título" />
+				<input
+					placeholder="Título"
+					value={title}
+					onChange={(event) => setTitle(event.target.value)}
+				/>
 
-				<input type="number" placeholder="Valor" />
+				<input
+					type="number"
+					placeholder="Valor"
+					value={value}
+					onChange={(event) => setValue(Number(event.target.value))}
+				/>
 
-				<input placeholder="Categoria" />
+				<TransactionTypeContainer>
+					<RadioBox
+						type="button"
+						onClick={() => setType('deposit')}
+						isActive={type === 'deposit'}
+						activeColor="green"
+					>
+						<img src={incomeImg} alt="Entrada" />
+						<span>Entrada</span>
+					</RadioBox>
+
+					<RadioBox
+						type="button"
+						onClick={() => setType('withdraw')}
+						isActive={type === 'withdraw'}
+						activeColor="red"
+					>
+						<img src={outcome} alt="Saída" />
+						<span>Saída</span>
+					</RadioBox>
+				</TransactionTypeContainer>
+
+				<input
+					placeholder="Categoria"
+					value={category}
+					onChange={(event) => setCategory(event.target.value)}
+				/>
 
 				<button type="submit">Cadastrar</button>
 			</Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { Model, createServer } from 'miragejs';
 import { App } from './App';
 
+//criando conexão com rotas de API fictcia para teste/dev
 createServer({
+	models: {
+		transaction: Model,
+	},
+
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: 'Desenvolvimento de website',
+					type: 'deposit',
+					category: 'Desenvolvimento',
+					amount: 12000,
+					createdAt: new Date('2022-03-31 10:00:00'),
+				},
+				{
+					id: 2,
+					title: 'Aluguel',
+					type: 'withdraw',
+					category: 'Casa',
+					amount: 1200,
+					createdAt: new Date('2022-04-05 14:00:00'),
+				},
+				{
+					id: 3,
+					title: 'Parcela do Carro',
+					type: 'withdraw',
+					category: 'Automóvel',
+					amount: 800,
+					createdAt: new Date('2022-06-05 12:00:00'),
+				},
+			],
+		});
+	},
+
 	routes() {
 		this.namespace = 'api';
 
 		this.get('/transactions', () => {
-			return [
-				{
-					id: 1,
-					title: 'Transactions 1',
-					amount: 400,
-					type: 'deposit',
-					category: 'Food',
-					createdAt: new Date(),
-				},
-			];
+			return this.schema.all('transaction');
+		});
+
+		this.post('/transactions', (schema, request) => {
+			const data = JSON.parse(request.requestBody);
+
+			//schema - data base
+			return schema.create('transaction', data);
 		});
 	},
 });
